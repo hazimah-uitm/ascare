0bf7f184d3ed266019b6f85487cb4a5c53c91386
hazimah-uitm <hazimahpethie@uitm.edu.my>
2024-12-13 11:05:50 +0800

    change bg style

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -3,8 +3,8 @@
         <img src="{{ asset('public/assets/images/logo-icon.png') }}" class="logo-icon" alt="logo icon">
     </div>
     <div>
-        <h4 class="logo-text">SISTEM ZAKAT</h4>
-        <!-- <h6 class="logo-subtitle">Template System</h6> -->
+        <h4 class="logo-text">ASCARE</h4>
+        <h6 class="logo-subtitle">Asnaf Siswa Care</h6>
     </div>
     <div class="toggle-icon ms-auto" id="toggle-icon"><i class='bx bx-arrow-to-left'></i></div>
 </div>
